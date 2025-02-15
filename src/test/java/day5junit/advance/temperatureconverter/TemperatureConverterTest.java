42dd87d6a459541bcf15bcfeec6d14d32168b65c
amanglani2503 <ambermanglani8871@gmail.com>
2025-02-15 16:01:52 +0530

    [Amber] Add. Implemented and tested temperature converter

---
+++ src/test/java/day5junit/advance/temperatureconverter/TemperatureConverterTest.java
@@ -0,0 +1,21 @@
+package day5junit.advance.temperatureconverter;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TemperatureConverterTest {
+
+    @Test
+    void testCelsiusToFahrenheit() {
+        assertEquals(32.0, TemperatureConverter.celsiusToFahrenheit(0), 0.001);
+        assertEquals(212.0, TemperatureConverter.celsiusToFahrenheit(100), 0.001);
+        assertEquals(77.0, TemperatureConverter.celsiusToFahrenheit(25), 0.001);
+    }
+
+    @Test
+    void testFahrenheitToCelsius() {
+        assertEquals(0.0, TemperatureConverter.fahrenheitToCelsius(32), 0.001);
+        assertEquals(100.0, TemperatureConverter.fahrenheitToCelsius(212), 0.001);
+        assertEquals(25.0, TemperatureConverter.fahrenheitToCelsius(77), 0.001);
+    }
+}
